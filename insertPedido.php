1b16459fe00702836297e4829a247ecf77ea1f80
Jokabed Hernandez <jokhebra@hotmail.com>
2022-10-30 17:48:26 -0600

    Manejo de sesión para crear pedido

--- insertPedido.php
+++ insertPedido.php
@@ -4,8 +4,11 @@ extract($_REQUEST);
 $mysqli = new mysqli("localhost", "root", "", "pizzeria");
 $resultado = $mysqli->query("SELECT * FROM pizzas where  id_pizzas=$id_pizza"); // busco la pizza que tiene el id que se seleccionó para saber el precio
 $pizza = $resultado->fetch_assoc(); // obtengo la pizza
+// Extraigo el rol del usuario de la sesión para insertarlo en el query de pedido
+session_start();
+$id_usuario = $_SESSION['id'];
 // // Inserto el pedido
-$pedido = $mysqli->query("INSERT INTO `pedidos`(`nombre_persona`, `total`, `fecha`, `id_usuario`) VALUES ('$nombre_persona', $pizza[precio],'0000-00-00 00:00',1)");
+$pedido = $mysqli->query("INSERT INTO `pedidos`(`nombre_persona`, `total`, `fecha`, `id_usuario`) VALUES ('$nombre_persona', $pizza[precio],'0000-00-00 00:00',$id_usuario)");
 $nuevoPedido = $mysqli->query("SELECT * FROM pedidos order by id_pedidos desc limit 1"); // Busco el pedido creado
 $ped = $nuevoPedido->fetch_assoc(); // obtengo el pedido creado
 // // Inserto mi pizza del pedido
